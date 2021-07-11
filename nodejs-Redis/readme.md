13ba74a0b277b0402d642d56d70277610b8f0ac9
kamiaiqiaodaima <954108736@qq.com>
2021-07-11 21:29:14 +0800

    redis学习和在nodeJS中的应用

---
+++ nodejs-Redis/readme.md
@@ -0,0 +1,17 @@
+
+### 下载依赖
+```
+npm install 
+```
+
+### 启动服务
+```
+node server
+```
+
+### 测试请求
+直接请求 localhost:7788/photos 即可
+
+
+具体文档链接：
+<a href="http://note.youdao.com/noteshare?id=a5a5e2bfe41a9ad0905e7795ce296f5b&sub=50840C18F28145EC95DDF3583B2F5652">Redis在nodeJS中的应用</a>
---
+++ nodejs-Redis/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+const Redis = require("redis");
+
+const redisClient = Redis.createClient()
+const defaultExpire = 3000; // 缓存时间
+
+const app = express();
+app.use(cors());
+
+app.get("/photos", async (req, res) => {
+    const albumId = req.query.albumId;
+    // 先查询 Redis 是否有数据
+    redisClient.get("photos",async (err, data) => {
+        if (err) console.error(err);
+        // 有就直接响应
+        if (data != null) {
+            return res.json(JSON.parse(data))
+        } else {
+            // 没有就重新请求
+            const {
+                data
+            } = await axios.get(
+                "http://jsonplaceholder.typicode.com/photos", {
+                    params: {
+                        albumId
+                    }
+                }
+            )
+            redisClient.setex("photos", defaultExpire, JSON.stringify(data))
+            res.json(data);
+        }
+    })
+})
+app.listen(7788, () => {
+    console.log("server is running")
+});
