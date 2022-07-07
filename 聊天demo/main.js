9549945cc54aa9fe86db74d49dda9c7e7b266160
kami <52963003+LuoHongXin@users.noreply.github.com>
2022-07-07 15:16:43 +0800

    Delete 聊天demo directory

--- 聊天demo/main.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const port = 9528;
-const app = express();
-app.use(express.static("./"))
-app.listen(port, () => {
-    console.log(port + "server is runnning")
-})
